ba5cc167d58d0d7ca3c607895d8e97f3cd480636
Shivansh <shiv10ansh@gmail.com>
2024-04-10 07:46:28 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
