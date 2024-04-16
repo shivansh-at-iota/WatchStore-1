6e6771f027b17a91b56e44eafde2818ed137fa63
Shivansh Srivastava <shivansh.iota@gmail.com>
2024-04-16 13:55:02 +0530

    Change

--- src/BrandsLogo.jsx
+++ src/BrandsLogo.jsx
@@ -10,18 +10,18 @@ function BrandsLogo() {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 3000,
-    autoplaySpeed: 3000,
+    speed: 2000,
+    autoplaySpeed: 2000,
     cssEase: "linear"
   };
   return (
     <Box className="slider-container">
       <Slider {...settings}>
-        <Box component="img" src="./Images/lokamt.png" alt="LOKAMT"/>
-        <Box component="img" src="./Images/financialexpress.png" alt="financialexpress"/>
-        <Box component="img" src="./Images/abp.png" alt="abp"/>
-        <Box component="img" src="./Images/silicon.png" alt="silicon"/>
-        <Box component="img" src="./Images/rewired.png" alt="rewired"/>
+        <Box component="img" src="./Images/lokamt.png" alt="LOKAMT" />
+        <Box component="img" src="./Images/financialexpress.png" alt="financialexpress" />
+        <Box component="img" src="./Images/abp.png" alt="abp" />
+        <Box component="img" src="./Images/silicon.png" alt="silicon" />
+        <Box component="img" src="./Images/rewired.png" alt="rewired" />
       </Slider>
     </Box>
   );
